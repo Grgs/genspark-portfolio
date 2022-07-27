c2aef134aac0e0f39ba3bc33c2ed4eaf3277c2f7
Michael Guirguis <guirguis@mguirguis.com>
2022-07-27 12:00:29 -0400

    moved images to the right of the card

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -14,18 +14,25 @@ export default function Project(project: ProjectType) {
 
 
   return (
-    <div className="card bg-light mb3">
-
-      <div className="card-body" onClick={handleClick}>
-        {project.image && <img className="card-img-top" src={project.image} alt={project.name} />}
-        <h5 className="card-title">{project.name}</h5>
-        <p className="card-text">{project.description}
-          {!showMore && project.more && <a href="#" className="float-right">  more...</a>}
-        </p>
-        {showMore && project.more}
-        <a href={project.URL} className="btn btn-primary">
-          View on Github
-        </a>
+    <div className="card">
+      <div className="row g-0">
+        <div className="col-sm-5">
+          {project.image && <img className="card-img-top row" src={project.image} alt={project.name} />}
+        </div>
+        <div className="col-sm-7">
+          <div className="bg-light mb3">
+            <div className="card-body" onClick={handleClick}>
+              <h5 className="card-title">{project.name}</h5>
+              <p className="card-text">{project.description}
+                {!showMore && project.more && <a href="#" className="float-right">  more...</a>}
+              </p>
+              {showMore && project.more}
+              <a href={project.URL} className="btn btn-primary">
+                View on Github
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
