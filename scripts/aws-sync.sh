e3443971ff521467c494afca800855b66d230561
Michael Guirguis <guirguis@mguirguis.com>
2022-07-27 09:36:58 -0400

    updated portfolio

--- scripts/aws-sync.sh
+++ scripts/aws-sync.sh
@@ -2,4 +2,4 @@
 # get the current script directory
 BASEDIR=$(dirname "$0")
 #publish to aws s3
-aws s3 sync "$BASEDIR"/../build/ s3://mgrgs.com/portfolio --delete
+aws s3 sync "$BASEDIR"/../build/ s3://mgrgs.com --delete
--- src/Components/MainBody.tsx
+++ src/Components/MainBody.tsx
@@ -8,6 +8,11 @@ const projects = [
     description: "A tile-based game, where the player controls a human character and must navigate through a grid, avoiding or fighting the goblin along the way.",
   },
   {
+    URL: "https://github.com/Grgs/HyperskillAccountService",
+    name: "Hyperskill Account Service",
+    description: "This is a Rest service that allows you to create an account and login to your account. It also manages lockout after repeated authentication failure.",
+  },
+  {
     URL: "https://github.com/Grgs/NumbersToWords",
     name: "Words to Numbers",
     description: "This program converts numbers to English words. It can convert the numbers to the modern English, traditional long scale British, traditional long scale European and Indian English number systems.",
@@ -28,6 +33,11 @@ const projects = [
     URL: "https://github.com/Grgs/Hangman",
     name: "Hangman",
     description: "A word guessing game."
+  },
+  {
+    URL: "https://github.com/Grgs/LS",
+    name: "ls",
+    description: "A simple command line interface for listing files and directories in any operating system that can run Python."
   },
   {
     URL: "https://github.com/Grgs",
